ea10942ef936ca48c91f6e3c5f344badffe2271c
fkbsco <francheskadlcrz@gmail.com>
2020-09-28 21:37:59 +0800

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,49 @@
+import { useState, useEffect } from 'react'
+import { Container } from 'react-bootstrap'
+import { UserProvider } from '../UserContext'
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+export default function App({ Component, pageProps }) {
+  const [user, setUser] = useState({
+      id: null
+  })
 
-export default MyApp
+  useEffect(() => {
+      fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users/profile`, {
+          headers: {
+              Authorization: `Bearer ${localStorage.getItem('token')}`
+          }
+      })
+      .then(res => res.json())
+      .then(data => {
+          if(data._id){
+              setUser({
+                  id: data._id
+              })
+          }else{
+              setUser({
+                  id: null
+              })
+          }            
+      })
+  }, [user.id])
+
+  const unsetUser = () => {
+      localStorage.clear()
+
+      setUser({
+          id: null
+      });
+  }
+
+  return (
+      <React.Fragment>
+          <UserProvider value={{user, setUser, unsetUser}}>
+              <Container>
+                  <Component {...pageProps} />
+              </Container>
+          </UserProvider>
+      </React.Fragment>
+  )
+}
