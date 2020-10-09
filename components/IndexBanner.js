c1abd753155ea44b0a7a6357cb809a1bec3057d6
fkbsco <francheskadlcrz@gmail.com>
2020-10-09 18:20:39 +0800

    img links

--- components/IndexBanner.js
+++ components/IndexBanner.js
@@ -5,10 +5,10 @@ export default function IndexBanner() {
     return (
         <React.Fragment>
         <div className="webban">
-            <center><img class="webanner" src="https://lh3.googleusercontent.com/ukRBtINFMcLHgMOIlWvewVk942PjrxFRAIcRXAFHQKKZQko6d92bdcuf_RsCiGEnS_RqvLJkTmB8F6QTh5ShLncYuN40-d_iY_nAOjrUckP7KSweTwrjpYFHULHPUlV8QOf1lxnu_3htYC5Le9VayBJJdkxaASRvAFVJiPeWDcf_oRheVxJxDHn-oarDoAsB-q-LZ-WchrQEzC8QZE-rVLLGIZUTycOhZKHVhJnTBMSvrLgEs4YMc7U8wnzZdAzP8pSv5dY7yrCGZdKnSIOzUOLrKsSqJhnsIlqGzGxIjGWjv2tEsLybFPdnxB94euA7uXQBWBZWBfPRzV_nrYSWvEEFGlL98iupdhVQEGhQ54kTLMT9u9exzdylFeHCJHfb7Y2PZK8n5NCno1RqtBhB0Hee5gCMGKo3pUAWr70WBwJv2IeJRF5yyinTbryQxYKzpc9-0fqiCNRCVap1-OZ5im1zaT5JvCj90XsCDwzDhALIlLF-AU93V81RgtNdg2XB_yAEQgKm6MOtO1lMyrttJ1tOJ0IqkH5vxqSaCRL-wovrHHTsAayKyj_noAfTjELcvDqul2Pv8RRhzZj4xTPzI6Ni2EXBEDyJk3yqJ8F2cBrd2Rh15k9AMBn-L_crm3k4jkrQHkeL-AMy-TmtbcIQTVLH60x2C4-OltlxxQRFmxPq20T5XFgFI3zB3DL9=w1920-h732-no?authuser=0" /></center>
+            <center><img class="webanner" src="https://i.imgur.com/2QSRBu7.png" /></center>
         </div>
         <div className="mobban">
-            <center><img class="mobanner" src="https://lh3.googleusercontent.com/-fSrjFBavPTmljeAgprvfDu_t476UuHW8uOG3_i-PXmWOWuuQ1RLKgzuKWf25H39gnRnUfMMg5VgNugDx-Vc-T7rpsmVRH6NIQGIPRvUAGUvmIeK-ueQN8Ewod0fiwjV2wSioDDs5Mup-wTSd3uCs9WCqJhqfApDUpok73e8Rt0SVBYCsZ4kCF7f4DvPmzOgfWdRaHMB97Et3Ef0-jY0icED-tEE9tYoDpia-qhB9FlduzEQShEn-0t5Zr2-LmqTkf-P8VUAGQxrqX1KIPUF2o5qgq1ZOv6hec83g-ptPAZB86Dc9ClhgZSZAvtOkOcSCgHxhkaviPQPyO3kucFLtOyrl-7O9n0v47qc6q2IYV2RmbXiZzP0NOyV7j9XCHOwIxr_-XwJIugo_uGond6nHogrmE9AcGD_KaF7oXLk9LwBMzjAsM0QhMRUnQ3aBWWYKROR0WRQ2ythB0P7hV0yoFkm3_tHqyIR7YqgdzZQkyx0F0Tv5386bRGGkVtdHUTj_NppODFbUYBBfqTkoF4uyksRGhw6iVFbmeegCrahC1keIyiIz6kJtzvIwq8LhMFP6cCnnsUOPZVwH9X914FvmlCzoO8Sf0jWkpUPMDTZUHO1tmuLvTmI014CTgYtJmp1BWX88q4ZJUU79izqc5fwiIFkxTPpkL4NSmXHFrRP6EGNu93kJQ69X55hCgF_=w668-h943-no?authuser=0" /></center>
+            <center><img class="mobanner" src="https://i.imgur.com/pOz7SAl.png" /></center>
         </div>
         </React.Fragment>
     )
